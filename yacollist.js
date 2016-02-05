de0ab77e5270c96bd6e66a3ed557de40c2d2fff8
Etienne <etienne@ThinkPax>
2016-02-05 15:59:52 -0500

    feat: Add isString function

--- yacollist.js
+++ yacollist.js
@@ -3,4 +3,13 @@
  Code:
  */
 
+/**
+ * Test if the passed argument is a String.
+ * @param {Object} s - A variable to test.
+ * @returns {bool} True if a String.
+ */
+function isString(s) {
+    return (s instanceof String || typeof s === 'string');
+}
+
 /* yacollist.js ends here */
