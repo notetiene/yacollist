0882501bcaa5d9eaebb23ddc5fce1564346908b1
Etienne <etienne@ThinkPax>
2016-02-05 16:00:40 -0500

    feat: Add isArray function

--- yacollist.js
+++ yacollist.js
@@ -12,4 +12,13 @@ function isString(s) {
     return (s instanceof String || typeof s === 'string');
 }
 
+/**
+ * Test if the passed argument is an Array and has values.
+ * @param {Object} a - A variable to test.
+ * @returns {bool} True if an Array.
+ */
+function isArray(a) {
+    return (a instanceof Array && a.length > 0);
+}
+
 /* yacollist.js ends here */
