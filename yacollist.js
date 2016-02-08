60b28dbecadc5a509764dc8dbe3b288fc69033e7
Etienne <etienne@ThinkPax>
2016-02-08 15:37:14 -0500

    feat: Add custom Error NotArray

--- yacollist.js
+++ yacollist.js
@@ -65,10 +65,34 @@ function NotStringError(message) {
 NotStringError.prototype = new TypeError;
 
 /**
- * Error that a value is not of type {@linkcode String[]}.
+ * Error that a value is not of type {@linkcode Array}.
  * @constructor
  * @param {String} message - Message to display.
  * @augments TypeError
+ */
+function NotArrayError(message) {
+    /**
+     * @property {String} name - Name for the type of error.
+     * @constant
+     * @public
+     */
+    this.name = 'NotArrayStringsError';
+
+    /**
+     * @property {String} message - Human-readable description of the error.
+     * @constant
+     * @public
+     */
+    this.message = message || 'It’s not a valid Array';
+    this.stack = (new Error()).stack;
+}
+NotArrayError.prototype = new TypeError;
+
+/**
+ * Error that a value is not of type {@linkcode String[]}.
+ * @constructor
+ * @param {String} message - Message to display.
+ * @augments NotArrayError
  */
 function NotArrayStringsError(message) {
     /**
@@ -86,7 +110,7 @@ function NotArrayStringsError(message) {
     this.message = message || 'It’s not a valid Array of Strings';
     this.stack = (new Error()).stack;
 }
-NotArrayStringsError.prototype = new TypeError;
+NotArrayStringsError.prototype = new NotArrayError;
 
 /* ============================================================ */
 /* Utility functions ends here                                  */
