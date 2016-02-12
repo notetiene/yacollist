69e1e398e38c07eef4f8fd8197fea35ee06f4b50
Etienne <etienne@ThinkPax>
2016-02-11 19:11:39 -0500

    refactor: Throw an exception instead of undefined
    Returning undefined in a constructor won't destroy the Object, but
    return an Object undefined.

--- yacollist.js
+++ yacollist.js
@@ -187,7 +187,7 @@ var ToggleList = function(data) {
     for(var i=0, len=data.length; i<len; i++) {
         // Always supposed to be String, because we increment i when next is subskill
         if(!isString(data[i])) {
-            return undefined;
+            throw NotStringError('First cell or cell after an Array must should be a String.');
         }
         // Verify that i+1 won't be out of bound
         else if(i+1 >= len) {
@@ -242,5 +242,7 @@ ToggleList.prototype.addSubList = function(title, subItems) {
     }
     return false;
 };
+
+
 
 /* yacollist.js ends here */
