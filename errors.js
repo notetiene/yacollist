953491a365d2e7f859d0205078b1581f5d7cfb20
Etienne <etienne@ThinkPax>
2016-02-13 18:43:03 -0500

    refactor: Move custom Errors to its own file

---
+++ errors.js
@@ -0,0 +1,79 @@
+/* errors.js --- Custom errors for Yet Another Expand/Collapse List (yacollist)
+ Commentary:
+ Code:
+ */
+
+
+/**
+ * Error that a value is not of type {@linkcode String}.
+ * @constructor
+ * @param {String} message - Message to display.
+ * @augments TypeError
+ */
+function NotStringError(message) {
+    /**
+     * @property {String} name - Name for the type of error.
+     * @constant
+     * @public
+     */
+    this.name = 'NotStringError';
+
+    /**
+     * @property {String} message - Human-readable description of the error.
+     * @constant
+     * @public
+     */
+    this.message = message || 'It’s not a valid String.';
+    this.stack = (new Error()).stack;
+}
+NotStringError.prototype = new TypeError;
+
+/**
+ * Error that a value is not of type {@linkcode Array}.
+ * @constructor
+ * @param {String} message - Message to display.
+ * @augments TypeError
+ */
+function NotArrayError(message) {
+    /**
+     * @property {String} name - Name for the type of error.
+     * @constant
+     * @public
+     */
+    this.name = 'NotArrayStringsError';
+
+    /**
+     * @property {String} message - Human-readable description of the error.
+     * @constant
+     * @public
+     */
+    this.message = message || 'It’s not a valid Array.';
+    this.stack = (new Error()).stack;
+}
+NotArrayError.prototype = new TypeError;
+
+/**
+ * Error that a value is not of type {@linkcode String[]}.
+ * @constructor
+ * @param {String} message - Message to display.
+ * @augments NotArrayError
+ */
+function NotArrayStringsError(message) {
+    /**
+     * @property {String} name - Name for the type of error.
+     * @constant
+     * @public
+     */
+    this.name = 'NotArrayStringsError';
+
+    /**
+     * @property {String} message - Human-readable description of the error.
+     * @constant
+     * @public
+     */
+    this.message = message || 'It’s not a valid Array of Strings.';
+    this.stack = (new Error()).stack;
+}
+NotArrayStringsError.prototype = new NotArrayError;
+
+/* errors.js ends here */
